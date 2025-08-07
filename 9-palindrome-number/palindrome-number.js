ba997c53d8fc702d4db5fbb205e57e4290b12259
Shaik Dawood <77268355+dawoodxp97@users.noreply.github.com>
2025-08-07 10:32:37 +0530

    Time: 6 ms (67.11%) | Memory: 64.4 MB (51.83%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -7,13 +7,9 @@ var isPalindrome = function(x) {
     let rev = 0;
     while(x > 0){
         let remainder = x % 10;
-        console.log(remainder)
         rev = (10 * rev) + remainder;
-        console.log(rev)
         x = Math.floor(x / 10);
-        console.log(x)
     }
-    console.log(x, rev, "RR")
     if(n === rev){
         return true;
     }else{
